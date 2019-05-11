85e8b5ba4e6d93ed5920192e600c1546389c9103
Maicon <maincon.brandao@gmail.com>
2019-05-10 22:03:13 -0300

    Preparando para deixar online

--- GAMES_2019_T1/export/html5/haxe/ApplicationMain.hx
+++ GAMES_2019_T1/export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "6";
+		app.meta["build"] = "10";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "GAMES_2019_T1";
 		app.meta["name"] = "GAMES_2019_T1";
